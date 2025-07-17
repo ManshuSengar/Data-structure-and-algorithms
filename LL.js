05e2060616aca0fe1375020e735247ff6bf826aa
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-07-17 20:05:11 +0530

    Update LL.js

--- LL.js
+++ LL.js
@@ -147,3 +147,43 @@ var hasCycle = function(head) {
     return false;
 };
 
+
+
+#234. Palindrome Linked List - https://leetcode.com/problems/palindrome-linked-list/description/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let slow=fast=head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+
+   
+    let prev=null;
+    let curr=slow;
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=temp;
+    }
+
+    let startList=head;
+    let endList=prev;
+    while(endList){
+        if(endList.val!=startList.val) return false;
+        endList=endList.next;
+        startList=startList.next;
+    }
+    return true;
+};
