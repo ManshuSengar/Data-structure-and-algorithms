3ad63be4c56bbde0a7ce6cc15f8b11e0f88f7c97
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-07-17 06:57:30 +0530

    Update LL.js

--- LL.js
+++ LL.js
@@ -71,3 +71,27 @@ MyLinkedList.prototype.deleteAtIndex = function(index) {
     }
     this.size--;
 };
+
+
+middle of linked list 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    if(head==null) return;
+    let slow=head;
+    let fast=head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow;
+};
