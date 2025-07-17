4e6784305e49f865855232a2b59f990117a62313
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-07-17 07:09:07 +0530

    Update LL.js

--- LL.js
+++ LL.js
@@ -95,3 +95,28 @@ var middleNode = function(head) {
     }
     return slow;
 };
+
+
+#Reverse a LL
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev=null;
+    let curr=head;
+    while(curr){
+        let temp=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr =temp;
+    }
+    return prev;
+};
