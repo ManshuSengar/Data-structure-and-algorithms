0ba3fd95a2fb514af0d668789f7c54d1c08dae4f
Manshu Sengar <89942713+ManshuSengar@users.noreply.github.com>
2025-07-28 12:57:36 +0530

    Create jewlsandstone.js

---
+++ STRING/jewlsandstone.js
@@ -0,0 +1,15 @@
+var jewlsAndStone=function(jewel,stone){
+   let jSet=new set();
+   let count=0;
+   for(let i=0;i<jewel.length;i++){
+       jSet.add(j[i]);
+   }
+   
+   for(let j=0;j<stone.length;j++){
+       if(jSet.has(stone[i]){
+	     count++;
+	   }
+   }
+   
+   return count;
+}
